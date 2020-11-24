1fa307ead89be89a3cb4796e318de9480e348fae
Gustavo Costa <gus.nascimento.c@gmail.com>
2020-11-24 20:42:00 +0000

    populated database

---
+++ db/migrate/20201124200352_create_songs.rb
@@ -0,0 +1,23 @@
+class CreateSongs < ActiveRecord::Migration[6.0]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :artist
+      t.float :danceability
+      t.float :energy
+      t.integer :key
+      t.float :loudness
+      t.integer :mode
+      t.float :speechiness
+      t.float :acousticness
+      t.integer :instrumentalness
+      t.float :liveness
+      t.float :valence
+      t.float :tempo
+      t.integer :duration_ms
+      t.integer :time_signature
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+contents = File.read('track.json')
+foo = Kernel.eval(contents)
+
+i = 0
+
+while i < 100 do
+    Song.create(title: foo[i][:title], artist: foo[i][:artist], 
+                danceability: foo[i][:danceability], 
+                energy: foo[i][:energy], key: foo[i][:key], 
+                loudness: foo[i][:loudness], mode: foo[i][:mode], 
+                speechiness: foo[i][:speechiness], 
+                acousticness: foo[i][:acousticness], 
+                instrumentalness: foo[i][:instrumentalness], 
+                liveness: foo[i][:liveness], 
+                valence: foo[i][:valence], tempo: foo[i][:tempo], 
+                duration_ms: foo[i][:duration_ms], 
+                time_signature: foo[i][:time_signature])
+                
+    i = i + 1            
+end
+
+
