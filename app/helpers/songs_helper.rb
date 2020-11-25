8521327eb925ca8d396045fd1f21f87de114fc5b
Gustavo Costa <gus.nascimento.c@gmail.com>
2020-11-25 21:38:47 +0000

    before styles

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,15 +1,12 @@
 module SongsHelper
+    #sorts table based on given column
+    #requires a string entry in case column name and db key don't match 
     def sortable(column, title = nil)
+        #sets column title based on db key
         title ||= column.titleize
-        #css_class = (column == sort_column) ? "current #{sort_direction}" : nil
+        #sets the sorting direction
         direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-        link_to title, :sort => column, :direction => direction #{:class => css_class}
-    end
-    
-    def sortable_s(column, title = nil, search)
-        title ||= column.titleize
-        #css_class = (column == sort_column) ? "current #{sort_direction}" : nil
-        direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc"
-        link_to title, :sort => column, :direction => direction, :search => search #{:class => css_class}
+        #returns sorting link
+        link_to title, :sort => column, :direction => direction
     end
 end
